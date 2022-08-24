a1dcd7b5146f3e560b3b84c4a139781e16c6e0b5
gohellp <gohelp212003@gmail.com>
2022-08-24 10:57:57 +0300

    upd

--- platforms/soundcloud.js
+++ platforms/soundcloud.js
@@ -4,7 +4,7 @@ const scdl = require('soundcloud-downloader').default,
 module.exports={
 	name:"soundcloud",
 	execute(context){
-		if(context.update.message.text.match(/\/sets\//))return context.reply("This is not a track")
+		if(scdl.isPlaylistURL(context.update.message.text))return context.reply("This is not a track url")
 		scdl.getInfo(context.update.message.text, token_sc)
 			.then(async info=>{
 				scdl.download(context.update.message.text, token_sc)
